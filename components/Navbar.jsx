3762d976a878ffc0b799fab037c92d34a6f78315
PeriodicoHOYbo <135343550+PeriodicoHOYbo@users.noreply.github.com>
2024-04-10 22:23:16 -0400

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -54,8 +54,8 @@ export default function Navbar() {
             <div className={style.container}>
                 <nav className={`${style.nav}`} style={{ padding: '0 100px', position: 'relative' }}>
                     <Link href="/" legacyBehavior scroll={false}>
-                        <a className={` absolute pt-[10px] h-[50px] top-0 left-[0px] ${pathname == "#Sociedad" ? style.active : ''}`} onClick={handlerClick}>
-                            <span className=' h-[50px] rounded-full flex justify-center items-center bg-white rounded'><img src={'/inicio_v2.jpeg'} className="block h-[50px] rounded-full cursor-pointer" onClick={() => router.push('/')} alt="" /></span>
+                        <a className={` absolute pt-[5px] h-[40px] top-0 left-[0px] ${pathname == "#Sociedad" ? style.active : ''}`} onClick={handlerClick}>
+                            <span className=' h-[40px] rounded-full flex justify-center items-center bg-white rounded'><img src={'/inicio_v2.jpeg'} className="block h-[40px] rounded-full cursor-pointer" onClick={() => router.push('/')} alt="" /></span>
                         </a>
                     </Link>
                     <Link href="#Sociedad" legacyBehavior scroll={false}>
